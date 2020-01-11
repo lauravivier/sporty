0936f7ffa239289009de8e27192bd9644b796081
lauravivier <vivier.laura@gmail.com>
2020-01-11 16:31:27 +0100

    setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
    root to: "home#index"
 
-       <p class="notice"><%= notice %></p>
-       <p class="alert"><%= alert %></p>
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
