8f8c2d12de8fcc3a9156319096eb9ceec5a46158
lauravivier <vivier.laura@gmail.com>
2020-01-16 19:27:46 +0100

    adding model validations routes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,26 @@
 Rails.application.routes.draw do
+  get 'equipments/search'
   get 'users/show'
   get 'user/show'
 
   # lire toutes les offres
-  get_"equipments" => "equipments#index"
+  get "equipments" => "equipments#index"
 
   # lire une offre
-  get_"equipments/:id" => "equipments#show"
+  get "equipments/:id" => "equipments#show"
 
   # creer une offre
-  get_"equipments/new" => "equipments#new"
-  post_"equipments/" => "equipments#create"
+  get "equipments/new" => "equipments#new"
+  post "equipments/" => "equipments#create"
 
   # updater une offre
-  get_"equipments/id/edit" => "equipments#edit"
+  get "equipments/id/edit" => "equipments#edit"
   patch "restaurants/:id " => "equipments#update"
 
   # supprimer une offre
   delete "equipments:/id" => "equipments#destroy"
 
   # devise_for :users
-  root to: "home#index"
+  root to: "equipments#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
