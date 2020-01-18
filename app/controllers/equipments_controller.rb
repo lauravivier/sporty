0f2413e18cb760691810445456cc6b9af2cd5c32
lauravivier <vivier.laura@gmail.com>
2020-01-18 15:01:09 +0100

    2 pages view product

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,4 +1,9 @@
 class EquipmentsController < ApplicationController
-  def search
+  def show
+    @equipment = Equipment.find(params[:id])
+  end
+
+  def index
+    @equipments = Equipment.all
   end
 end
